b0adb891e47a537210effdb0d6b90c658d0a2b90
Avnish Singh <avnish.singh@timesinternet.in>
2026-02-27 11:39:48 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9085921874-cd434",
+  "appId": "1:376534713230:web:cee194a21a1c86481e9adf",
+  "apiKey": "AIzaSyCYH1GElaihpy9NeAcNp-0S0JGkId7uLtk",
+  "authDomain": "studio-9085921874-cd434.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "376534713230"
+};
